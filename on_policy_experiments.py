d81ee186779451117e214e6d3c3329b29228945e
mizolotu <mizolotu@jyu.fi>
2021-05-11 14:00:38 +0300

    11.5.2021

--- on_policy_experiments.py
+++ on_policy_experiments.py
@@ -8,6 +8,7 @@ from reinforcement_learning.gym.envs.classic_control.pendulum import PendulumEnv
 from reinforcement_learning.gym.envs.classic_control.continuous_mountain_car import Continuous_MountainCarEnv
 from reinforcement_learning.gym.envs.box2d.lunar_lander import LunarLanderContinuous
 from reinforcement_learning.gym.envs.box2d.bipedal_walker import BipedalWalker
+from reinforcement_learning.common.callbacks import CheckpointCallback
 
 from reinforcement_learning.common.vec_env.subproc_vec_env import SubprocVecEnv
 from reinforcement_learning.common.policies import policy_0, policy_1, policy_2
@@ -28,7 +29,7 @@ algorithm_list = [
 ]
 
 policy_list = [
-    policy_0,
+    #policy_0,
     policy_1,
     policy_2
 ]
@@ -64,7 +65,7 @@ if __name__ == '__main__':
     parser.add_argument('-e', '--env', help='Environment index', type=int, default=0)
     parser.add_argument('-n', '--nenvs', help='Number of environments', type=int, default=4)
     parser.add_argument('-s', '--steps', help='Number of episode steps', type=int, default=64)
-    parser.add_argument('-u', '--updates', help='Number of updates', type=int, default=10000)
+    parser.add_argument('-u', '--updates', help='Number of updates', type=int, default=10)
     parser.add_argument('-a', '--algorithm', help='RL algorithm index', type=int, default=1)
     parser.add_argument('-o', '--output', help='Output directory', default='models')
     parser.add_argument('-c', '--cuda', help='Use CUDA', default=False, type=bool)
@@ -113,4 +114,5 @@ if __name__ == '__main__':
         format_strs = os.getenv('', 'stdout,log,csv').split(',')
         logger.configure(os.path.abspath(logdir), format_strs)
         model = algorithm(policy, env, n_steps=args.steps, verbose=1)
-        model.learn(total_timesteps=totalsteps)
+        cb = CheckpointCallback(args.steps*args.updates, logdir, verbose=1)
+        model.learn(total_timesteps=totalsteps, callback=cb)
